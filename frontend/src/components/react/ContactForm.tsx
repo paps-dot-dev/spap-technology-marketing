5bc79326728397ae44679d188bb02ef91cb4d94e
Shawn <78662682+paps-dot-dev@users.noreply.github.com>
2024-09-21 16:08:52 -0400

    desktop formatting and form routing

--- frontend/src/components/react/ContactForm.tsx
+++ frontend/src/components/react/ContactForm.tsx
@@ -3,63 +3,112 @@ import { useState } from 'react';
 
 interface FormDataProps {
 	firstName: string;
-	lastName: string;
 	email: string;
-	phone: string;
-	projectScope: String;
+	projectScope: string;
+	campaignCategory: string;
 	electronicConsent: boolean;
+	signUpForNewsletter: string;
 }
 
 const ContactForm: React.FC = () => {
 	const [formData, setFormData] = useState<FormDataProps>({
 		firstName: '',
-		lastName: '',
 		email: '',
-		phone: '',
 		projectScope: '',
+		campaignCategory: '',
 		electronicConsent: true,
+		signUpForNewsletter: 'Yes',
 	});
 
 	const handleInputChange = (e) => {
-		console.log(e);
+		e.preventDefault();
+		setFormData((prev) => ({
+			...prev,
+			[e.target.id]: e.target.value,
+		}));
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		let newSubData = {
+			email: formData.email,
+			firstName: formData.firstName,
+			groups: ['Welcome New Subscriber', 'Website Subscribers'],
+			trigger_automation: true,
+		};
+		try {
+			//move this to api folder
+			const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+			const targetUrl = 'https://api.sender.net/v3/subscribers';
+
+			const response = await fetch(proxyUrl + targetUrl, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${import.meta.env.PUBLIC_SENDER_TOKEN}`,
+					'x-requested-with': 'XMLHttpRequest',
+				},
+				body: JSON.stringify(newSubData),
+			});
+
+			if (response.ok) {
+				const data = await response.json();
+				console.log('Response:', data);
+			}
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	return (
-		<div className="mt-8 w-full">
-			<form className="flex flex-col gap-2 bg-white p-5 rounded-md w-full">
-				<label className="text-xs">
+		<div className="mt-8 w-full lg:mt-24 lg:px-96">
+			<form
+				onSubmit={handleSubmit}
+				className="flex flex-col gap-4 text-white p-5 rounded-md w-full">
+				<label className="text-md">
 					First Name
 					<input
+						id="firstName"
+						onChange={handleInputChange}
 						required
 						type="text"
 						placeholder="First Name"
-						className="p-4 block text-lg outline-none bg-indigo-100/50 w-full rounded-full"
+						className="p-4 block text-lg outline-none bg-white/25 w-full rounded-full"
 					/>
 				</label>
-				<label className="text-xs">
-					Last Name
-					<input
-						required
-						type="text"
-						placeholder="Last Name"
-						className="p-4 text-lg outline-none bloc bg-indigo-100/50 w-full rounded-full"
-					/>
-				</label>
-				<label className="text-xs">
+
+				<label className="text-md">
 					Email Address
 					<input
+						id="email"
+						onChange={handleInputChange}
 						required
 						type="email"
 						placeholder="Email Address"
-						className="p-4 text-lg outline-none block bg-indigo-100/50 w-full rounded-full"
+						className="p-4 text-lg outline-none block bg-white/25 w-full rounded-full"
 					/>
 				</label>
-				<label className="text-xs">
-					Tell Us a Little About the Project.
-					<textarea
-						placeholder="Brief Description of Project Scope"
-						className="p-4 text-md outline-none block bg-indigo-100/50 w-full rounded-md"
+
+				<label className="text-md">
+					Tell Me a Little About the Project.
+					<input
+						id="projectScope"
+						onChange={handleInputChange}
+						placeholder="I want to redesign my website."
+						className="p-4 text-lg outline-none block bg-white/25 w-full rounded-full"
 					/>
+				</label>
+				<label className="text-md">
+					I'm interested in more information on
+					<select
+						onChange={handleInputChange}
+						id="campaignCategory"
+						className=" p-4 text-lg outline-none block bg-white/25 w-full rounded-full">
+						<option value={'development'}>Modern Web Tools</option>
+						<option value={'marketing'}>Email Marketing</option>
+						<option value={'business'}>Business Tools</option>
+						<option value={'design'}>Web Design Tips</option>
+					</select>
 				</label>
 				<label className="text-xs mt-4">
 					<input
@@ -69,8 +118,22 @@ const ContactForm: React.FC = () => {
 						checked={formData.electronicConsent}
 						onChange={handleInputChange}
 					/>
-					I consent to receiving electronic communications from SPAP Technology
-					Solutions, including emails, phone calls, or text messages.
+					I consent to receiving newsletter and email communications from SPAP
+					Technology Solutions.
+				</label>
+				<label className="text-md bg-white  p-4 rounded-md bg-gradient-to-b from-indigo-500 to-violet-500 font-bold text-lg">
+					<span className="">Subscribe to 'Riffs. Code. Coffee.'</span>
+					<span className="block font-medium text-xs mb-4">
+						A free newsletter where I talk about music, tech, coffee, and
+						everything in between.
+					</span>
+					<select
+						onChange={handleInputChange}
+						id="signUpForNewsletter"
+						className=" p-4 mt-1 text-lg outline-none block bg-white/25 w-full rounded-full">
+						<option>Yes</option>
+						<option>No</option>
+					</select>
 				</label>
 
 				<button
