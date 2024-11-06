69db9250b01ed18099379dc23e04f286e634d16d
Shawn <78662682+paps-dot-dev@users.noreply.github.com>
2024-11-06 07:53:18 -0500

    added sitemap, meta, and testimonials

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -8,14 +8,18 @@ import icon from 'astro-icon';
 
 import netlify from '@astrojs/netlify';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://spaptechnology.com',
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
 		}),
 		react(),
 		icon(),
+		sitemap(),
 	],
 
 	output: 'server',
---
+++ frontend/src/components/react/TestimonialsContainer.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Button } from '../ui/button';
+
+const TestimonialsContainer = ({ testimonials }) => {
+	const maxLength = 300;
+
+	const truncateTestimonial = (text) => {
+		if (text.length > maxLength) {
+			return text.slice(0, maxLength) + '...';
+		}
+		return text;
+	};
+
+	const [isOpen, setIsOpen] = React.useState(false);
+
+	return (
+		<div className="py-10 w-full flex flex-wrap justify-center overflow-x-auto space-x-8 snap-x snap-mandatory">
+			{testimonials.map((item, index) => (
+				<div
+					key={index}
+					className="bg-text-primary/70 text-background-primary flex flex-col items-center justify-center rounded w-[500px] min-h-[300px] p-6 text-left flex-wrap space-y-4 snap-center">
+					<p>
+						{isOpen ? item.testimonial : truncateTestimonial(item.testimonial)}
+					</p>
+					<div className="flex justify-between items-center w-full space-y-2">
+						<Button
+							onClick={() => setIsOpen(!isOpen)}
+							size="sm"
+							variant="ghost">
+							{isOpen ? 'Read Less' : 'Read More'}
+						</Button>
+						<div>
+							<p className="text-right font-bold">{item.name}</p>
+							<p className="text-sm text-right font-extralight">
+								{item.company}
+							</p>
+						</div>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default TestimonialsContainer;
